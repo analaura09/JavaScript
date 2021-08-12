cbd372b18ff19e5136ca142c83630542fb4fa673
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-12 14:00:57 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
 var vel = 60.5
 console.log("A velocidade do seu carro é ${vel}km/h")
+if(vel > 60){
+    console.log('você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('dirija sempre usando cinto de segurança!')
