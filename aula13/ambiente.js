fd52720bf6c74e38be730519e192fcacb441414f
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-17 16:42:20 -0300

    repetições

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -2,22 +2,21 @@ var c = 1
 do{
     console.log(`passo ${c}`)
     c++ //c = c+1
-}while (c )
+}while (c <= 6)
 
 
-/*
+
 var c = 1
 while (c <= 3){
     console.log(`passo ${c}`)
     c++ //c = c+1
 } 
-*/
 
-/*
+
+
 console.log('Tudo bem?!')
 console.log('Tudo bem?!')
 console.log('Tudo bem?!')
 console.log('Tudo bem?!')
 console.log('Tudo bem?!')
 console.log('Tudo bem?!')
-*/
