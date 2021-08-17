1d516dfe1a0015c641e6af797e39b9c6815d7aca
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-17 16:40:40 -0300

    exercicio de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++ //c = c+1
+}while (c )
+
+
+/*
+var c = 1
+while (c <= 3){
+    console.log(`passo ${c}`)
+    c++ //c = c+1
+} 
+*/
+
+/*
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+*/
