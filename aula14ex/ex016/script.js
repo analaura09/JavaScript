b17b1676e0a887c3bc1754cf4f54feb1958095a0
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-19 17:09:24 -0300

    exercicio 15

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,19 @@
-alert('olá')
+function contar(){
+    var ini = document.getElementById('txtin')
+    var fi = document.getElementById('txtfi')
+    var pa = document.getElementById('txtpa')
+    var res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fi.value.length == 0 || pa.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    }else{
+        res.innerHTML= 'contando:'
+        var i = Number(ini.value)
+        var f = Number(fi.value)
+        var p = Number(pa.value)
+
+        for (var c = i; c <= f; c+=p){
+            res.innerHTML += `${c} \u{1F603} `
+        }
+    }
+}
