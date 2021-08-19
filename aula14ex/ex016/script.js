d594856b47288c99bb1ffe0e2be3ac4905bb31f5
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-19 19:34:13 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,16 +5,26 @@ function contar(){
     var res = document.getElementById('res')
 
     if(ini.value.length == 0 || fi.value.length == 0 || pa.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossíel contar'
     }else{
         res.innerHTML= 'contando:'
         var i = Number(ini.value)
         var f = Number(fi.value)
         var p = Number(pa.value)
 
-        for (var c = i; c <= f; c+=p){
-            res.innerHTML += ` ${c} \u{1F603} `
+        if(p <= 0){
+            window.aler
         }
-        res.innerHTML
+        if(i < f){
+            for (var c = i; c <= f; c+=p){
+            res.innerHTML += ` ${c} \u{1F449} `
+        }
+        }
+        else{
+            for(var c=i; c>=f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
