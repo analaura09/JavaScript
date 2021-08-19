41dad989c96456a150e5cdf9ef7eb8b3a98687d7
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-19 20:35:11 -0300

    exercicio 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+function tabuada(){
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        var n = Number(num.value)
+        var c = 1
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+   
+}
