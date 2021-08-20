9a6388fcd3bec989077501c085a760d8ce69203c
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-08-20 19:24:32 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+
+var num = [5,8,2,9,3]
+console.log(`nosso vetor é o ${num}`)
+//adicionar um numero na lista manualmente
+num[3]=6 
+//adicionar um numero usando o push
+num.push(7)
+//vendo quantos elementos há na lista pelo atributo length
+//colocando os numeros em ordem crescente com o sort
+console.log(`nosso vetor é o ${num}`)
+console.log(num.length)
+console.log(num.sort())
